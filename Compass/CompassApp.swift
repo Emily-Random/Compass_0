44aa253bf18f02b781688fece3f117f4e536158f
Emily Tang <emilyyunq@gmail.com>
2025-11-24 15:43:08 +0800

    Initial Commit

---
+++ Compass/CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassApp.swift
+//  Compass
+//
+//  Created by emily2 on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
